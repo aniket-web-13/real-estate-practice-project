15742f15f5e5202ac9aa2773fcb8c7e7ee8823d4
aniketweb13 <aniketweb13@gmail.com>
2025-11-03 18:07:25 +0530

    img agent

--- js/components/agentsCards.js
+++ js/components/agentsCards.js
@@ -1,51 +1,51 @@
 let agent = [
   {
-    src: "./assets/jpg/Denya-contadora.jpg",
+    src: "/assets/jpg/Denya-contadora.jpg",
     agentName: "Denya Contadora",
     agentPost: "Real estate investment specialist",
   },
   {
-    src: "./assets/jpg/Encarna-homie.jpg",
+    src: "/assets/jpg/Encarna-homie.jpg",
     agentName: "Encarna Homie",
     agentPost: "Commercial property agent",
   },
   {
-    src: "./assets/jpg/Agustín-trabajo.jpg",
+    src: "/assets/jpg/Agustín-trabajo.jpg",
     agentName: "Agustín trabajo",
     agentPost: "Lead property advisor",
   },
   {
-    src: "./assets/jpg/fardin-Sayyad.jpg",
+    src: "/assets/jpg/fardin-Sayyad.jpg",
     agentName: "Fardin Sayyad",
     agentPost: "Luxury property expert",
   },
   {
-    src: "./assets/jpg/Alday-banamex.jpg",
+    src: "/assets/jpg/Alday-banamex.jpg",
     agentName: "Alday Banamex",
     agentPost: "Residential sales consultant",
   },
   {
-    src: "./assets/jpg/Valerie-ahorro.jpg",
+    src: "/assets/jpg/Valerie-ahorro.jpg",
     agentName: "Valerie Ahorro",
     agentPost: "Listing agent",
   },
   {
-    src: "./assets/jpg/Cristela-estilista.jpg",
+    src: "/assets/jpg/Cristela-estilista.jpg",
     agentName: "Cristela Estilista",
     agentPost: "Luxury property expert",
   },
   {
-    src: "./assets/jpg/Maestra-yazmin.jpg",
+    src: "/assets/jpg/Maestra-yazmin.jpg",
     agentName: "Maestra Yazmin",
     agentPost: "Investment property consultant",
   },
   {
-    src: "./assets/jpg/Uziel-renta.jpg",
+    src: "/assets/jpg/Uziel-renta.jpg",
     agentName: "Uziel Renta",
     agentPost: "Senior real estate consultant",
   },
   {
-    src: "./assets/jpg/Bryce-jones.jpg.jpg",
+    src: "/assets/jpg/Bryce-jones.jpg.jpg",
     agentName: "Bryce Jones",
     agentPost: "Client relations manager",
   },
@@ -65,7 +65,7 @@ for (let i = 0; i < agent.length; i++) {
   agentCard.innerHTML = `
     
         <a href="#">
-            <img src="${element.src}" alt="${element.agentName}">
+            <img src=".${element.src}" alt="${element.agentName}">
         </a>
         <div class="agent-info">
             <a href="#"><h3>${element.agentName}</h3></a>
